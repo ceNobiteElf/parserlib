405e7784fdef4a449f30387d2c67fa40d3850cb3
Con Momberg <mombergcon@gmail.com>
2018-07-29 20:47:21 +0200

    [A] Started adding tests and gave ParserLibTests internal access to ParserLib to aid in testing.

--- ParserLib/Program.cs
+++ ParserLib/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-
-using ParserLib.Json;
 
 namespace ParserLib
 {
@@ -9,31 +6,6 @@ namespace ParserLib
 	{
 		static void Main(string[] args)
 		{
-			JsonObject obj = JsonParser.ParseFromFile<JsonObject>(@"D:\Libraries\Desktop\JsonTest\cars_test.json");
-
-			Console.WriteLine(obj.Count);
-
-			foreach (KeyValuePair<JsonString, JsonElement> pair in obj)
-			{
-				Console.WriteLine($"{pair.Key} {pair.Value}");
-			}
-
-			Console.WriteLine(obj["cars"][1]["name"]);
-
-			/*var obj = JsonParser.ParseFromString<JsonObject>("{ 'name' : 'Con' }");
-
-			Console.WriteLine(obj.Count);
-			Console.WriteLine(obj["name"]);*/
-
-			/*var obj2 = JsonParser.ParseFromString<JsonArray>("[{ 'name' : 'Con' }, { 'name' : 'Kevin' }, { 'name' : 'Stab'}]");
-
-			Console.WriteLine(obj2.Count);
-			Console.WriteLine(obj2[0]["name"]);*/
-
-			Console.WriteLine(JsonWriter.WriteToString(obj, true));
-
-			JsonWriter.WriteToFile(@"D:\Libraries\Desktop\JsonTest\cars_test.json", obj, true);
-
 			Console.ReadKey();
 		}
 	}
---
+++ ParserLibTests/Json/ParserTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ParserLib.Json;
+
+namespace ParserLibTests
+{
+	[TestClass]
+	public class ParserTests
+	{
+		[TestMethod]
+		public void ParseFromString_ValidJson()
+		{
+			var result = JsonParser.ParseFromString<JsonArray>("[{'name': 'Con', 'occupation': 'Developer'}, {'name': 'Kirsten', 'occupation': 'Programmer'}, {'name' : 'Robin', 'occupation': 'Engineer'}]");
+
+			Assert.AreEqual(3, result.Count);
+			Assert.AreEqual("Engineer", (string)result[2]["occupation"]);
+		}
+	}
+}
